405bb3fec487e4942c0898769b5dcc76e5ad4dbe
gem-dikshapopli <diksha.popli@geminisolutions.com>
2023-07-20 12:54:24 +0530

    First initial commit

--- src/test/java/implementation/Resources.java
+++
@@ -1,8 +0,0 @@
-package implementation;
-
-
-public class Resources {
-    public static String url="https://restful-booker.herokuapp.com/booking";
-    public static Object auth="Basic YWRtaW46cGFzc3dvcmQxMjM=";
-
-}
